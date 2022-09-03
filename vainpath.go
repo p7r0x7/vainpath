ba5771824527812bb4f0a1dcba65c7046c8d1662
Matthew R Bonnette <mattrbonnette@pm.me>
2022-09-03 11:25:35 -0400

    Preliminary release of v0.5.
    Performance improvements w/ panic conditions mitigated!
    
    New function Trim().

--- vainpath.go
+++ vainpath.go
@@ -9,33 +9,96 @@ import (
 
 // Copyright © 2022 Matthew R Bonnette. Licensed under a BSD-3-Clause license.
 
-const fullRuneWidth = "" +
-	// Valid or Overlong-Encoded range (2-6 bytes)
-	"\x02\x02\x02\x02\x02\x02\x02\x02\x02\x02\x02\x02\x02\x02\x02\x02" +
+const runeLen, runeSelf = "" +
+	"\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01" +
+	"\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01" +
+	"\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01" +
+	"\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01" +
+	"\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01" +
+	"\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01" +
+	"\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01" +
+	"\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01" +
+	"\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01" +
+	"\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01" +
+	"\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01" +
+	"\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01" +
+	"\x01\x01\x02\x02\x02\x02\x02\x02\x02\x02\x02\x02\x02\x02\x02\x02" +
 	"\x02\x02\x02\x02\x02\x02\x02\x02\x02\x02\x02\x02\x02\x02\x02\x02" +
 	"\x03\x03\x03\x03\x03\x03\x03\x03\x03\x03\x03\x03\x03\x03\x03\x03" +
-	"\x04\x04\x04\x04\x04\x04\x04\x04\x05\x05\x05\x05\x06\x06\x01\x01"
+	"\x04\x04\x04\x04\x04\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01",
+	uint8(194) /* This table aligns with utf8.first. */
 
-// Shorten formats inputs in a way similar to the fish shell's method of shortening paths in
+// Trim inputs to no more than n runes; the output will end in the tail specified if the input is
+// longer than n runes. Inputs are assumed to be valid UTF-8-encoded strings.
+func Trim(str, tail string, n int) (ret string) {
+	ret = str
+	defer func() {
+		recover()
+		return
+	}()
+
+	var t, w int /* Front to back */
+	for w := 0; len(tail[w:]) > 0; t++ {
+		if c := tail[w]; c < runeSelf {
+			w++
+		} else {
+			w += int(runeLen[c])
+		}
+	}
+	for i := n - t; len(str[w:]) > 0 && i > 0; i-- {
+		if c := str[w]; c < runeSelf {
+			w++
+		} else {
+			w += int(runeLen[c])
+		}
+	}
+	trimmed := str[:w]
+	for i := t; len(str[w:]) > 0 && i > 0; i-- {
+		if c := str[w]; c < runeSelf {
+			w++
+		} else {
+			w += int(runeLen[c])
+		}
+	}
+
+	if len(str[w:]) == 0 {
+		return /* because ret equals str and is shorter than or contains exactly n runes. */
+	} else if t >= n {
+		if t == n {
+			return tail /* because str and tail respectively contain more than and exactly n runes. */
+		}
+		for w = len(tail); n > 0; n-- { /* Back to front */
+			_, t = DecodeLastRuneInString(tail[:w])
+			w -= t
+		}
+		return tail[w:] /* because both str and tail each contain more than n runes. */
+	}
+	return trimmed + tail /* because str and tail respectively contain more than and less than n runes. */
+}
+
+// Simplify formats inputs in a way similar to the fish shell's method of shortening paths in
 // 'fish/functions/prompt_pwd.fish' and is properly Windows-sensitive; it will almost certainly not
 // return valid paths and should be used for vanity purposes only. Inputs are assumed to be valid
 // UTF-8-encoded strings.
-func Shorten(path string) string {
-	path = Clean(path)
+func Simplify(path string) (ret string) {
+	ret, path = path, Clean(path)
 	/* Bytes of ASCII and non-ASCII code points cannot be mistaken for each other. */
-	dex := strings.LastIndexByte(path, Separator) + 1 /* There will always be more bytes. */
-	if len(path) < 4 || dex < 3 {
+	n, dex := len(path), strings.LastIndexByte(path, Separator)+1 /* There will always be more bytes. */
+	if n < 4 || dex < 3 {
 		// The following already-cleaned paths cannot be any more shortened:
 		// a) paths smaller than 4 bytes
 		// b) paths without separators (dex of 0)
 		// c) paths where the only separator is the first or second character (dex of 1 or 2)
 		return path
 	}
+	defer func() {
+		recover()
+		return
+	}()
 
 	out, prefix, w := strings.Builder{}, path[:dex], 0
-	out.Grow(len(path) / 3) /* Minimizes re-allocation without expensive calculation. */
+	out.Grow(min(n>>3+n-dex, n)) /* Minimizes re-allocation without expensive calculation. */
 
-	/* For performance reasons, this section is more verbose than strictly necessary. */
 	if prefix[0] == Separator {
 		w++
 	}
@@ -46,21 +109,23 @@ func Shorten(path string) string {
 			continue
 		}
 		if !unicode.IsLetter(r) {
-			_, t = DecodeRuneInString(prefix[w:])
-			if w += t; prefix[w] == Separator {
-				w++
-				continue
+			if c := prefix[w]; c < runeSelf {
+				if w++; c == Separator {
+					continue
+				}
+			} else {
+				w += int(runeLen[c])
 			}
 		}
 		out.WriteString(prefix[:w])
 		for {
-			if c := prefix[w]; c < 192 {
+			if c := prefix[w]; c < runeSelf {
 				if c == Separator {
 					break
 				}
-				w++ /* Invalid leading bytes are skipped. */
+				w++ /* Invalid leading bytes begin invalid 1-byte runes. */
 			} else {
-				w += int(fullRuneWidth[c-192])
+				w += int(runeLen[c])
 			}
 		}
 		prefix, w = prefix[w:], 1
@@ -68,4 +133,11 @@ func Shorten(path string) string {
 
 	out.WriteString(path[dex-w:])
 	return out.String()
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
--- vainpath_test.go
+++ vainpath_test.go
@@ -1,110 +1,56 @@
 package vainpath
 
 import (
+	"fmt"
 	"math/rand"
 	. "path/filepath"
 	"strconv"
 	"strings"
 	"testing"
-	"time"
 	"unicode"
 	. "unicode/utf8"
 )
 
 // Copyright © 2022 Matthew R Bonnette. Licensed under a BSD-3-Clause license.
 
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
-
-func min(x, y int) int {
-	if x < y {
-		return x
-	}
-	return y
-}
-
+// randPath produces random valid strings of a given byte length.
 func randPath(size int) string {
 	var path strings.Builder
-	for path.Grow(size); size > 0; size-- {
-		if r := rand.Int31(); r%32 == 0 {
-			path.WriteByte(Separator)
-
-		} else if r%8 == 0 && size > 1 {
-			for r = 0; !unicode.IsPrint(r); {
-				switch min(4, size) {
-				case 4:
-					r = rand.Int31() & '\U0010FFFF'
-				case 3:
-					r = rand.Int31() >> 8 & '\uFFFF'
-				case 2:
-					r = rand.Int31() >> 16 & '\u07FF'
-				}
-			}
-			size -= RuneLen(r) - 1
-			path.WriteRune(r)
-
-		} else {
+	for path.Grow(size); size > 0; {
+		r := rand.Int31() % 100
+		switch {
+		case r > 10:
 			path.WriteByte(byte(rand.Int31()%95 + 32))
+			size--
+			continue
+		case r > 5 && size >= 2:
+			for !unicode.IsPrint(r) {
+				r = rand.Int31() >> 16 & '\u07FF'
+			}
+		case r > 1 && size >= 3:
+			for !unicode.IsPrint(r) {
+				r = rand.Int31() >> 8 & '\uFFFF'
+			}
+		case size >= 4:
+			for !unicode.IsPrint(r) {
+				r = rand.Int31() & '\U0010FFFF'
+			}
+		default:
+			continue
 		}
+		size -= RuneLen(r)
+		path.WriteRune(r)
 	}
-
 	return path.String()
 }
 
-func TestValidate(t *testing.T) {
-	space, log := strings.Repeat(" ", 40), strings.Builder{}
-
-	for i := 0; i < 1e5; i++ {
-		path := randPath(rand.Int() % 60)
-		if s, r := Shorten(path), refShorten(path); s != r {
-			if log.Len() < 4*1024 {
-				dex := strconv.Itoa(i)
-				for _, v := range [...]string{
-					"\n", space[:5-len(dex)], dex, "  ",
-					s, space[:40-min(40, RuneCountInString(s))], " \t",
-					r, space[:40-min(40, RuneCountInString(r))], " \t",
-					path,
-				} {
-					log.WriteString(v)
-				}
-			}
-			t.Fail()
-		}
+// refSimplify defines the expected behavior of Simplify.
+func refSimplify(path string) string {
+	if !ValidString(path) {
+		return path
 	}
-	if t.Failed() {
-		t.Log(log.String())
-	}
-}
-
-func BenchmarkAvgShorten(b *testing.B) {
-	const bestPath = string(Separator)
-	const worstPath = "\u07FF " + string(Separator)
-	fast := strings.Repeat(bestPath, b.N)
-	slow := strings.Repeat(worstPath, b.N/len(worstPath)+1)
-
-	b.SetBytes(int64(b.N))
-	b.ResetTimer()
-	for i := b.N; i > 0; i-- {
-		Shorten(fast[:i])
-		Shorten(slow[:i])
-	}
-}
-
-func BenchmarkRandShorten(b *testing.B) {
-	path := randPath(120)
-	b.SetBytes(120)
-	b.ReportAllocs()
-	b.ResetTimer()
-	for i := b.N; i > 0; i-- {
-		Shorten(path)
-	}
-}
-
-func refShorten(path string) string {
 	segments := strings.Split(Clean(path), string(Separator))
 
-	/* Skips final index */
 	for i, v := range segments[:len(segments)-1] {
 		if RuneCountInString(v) < 2 {
 			continue
@@ -120,4 +66,113 @@ func refShorten(path string) string {
 	}
 
 	return strings.Join(segments, string(Separator))
+}
+
+func TestForPanic(t *testing.T) {
+	t.Parallel()
+	for i := 0; i < 1024; i++ {
+		bytes := make([]byte, 1024)
+		rand.Read(bytes)
+		for i2 := range bytes[:len(bytes)-8] {
+			Trim(string(bytes[i2:i2+8]), "", 8)
+			Simplify(string(bytes[i2 : i2+8]))
+		}
+	}
+}
+
+// TestValidate tests if Simplify behaves identically to refSimplify for a sufficiently large number
+// of valid, variably-sized inputs.
+func TestIfValid(t *testing.T) {
+	t.Parallel()
+	space, log := strings.Repeat(" ", 40), strings.Builder{}
+
+	for i := 0; i < 1e5; i++ {
+		path := randPath(rand.Int() % 60)
+		if s, r := Simplify(path), refSimplify(path); s != r {
+			if log.Len() < 4*1024 {
+				dex := strconv.Itoa(i)
+				fmt.Fprint(&log, "\n", space[:5-len(dex)], dex, space[:2],
+					s, space[:40-min(40, RuneCountInString(s))], " \t",
+					r, space[:40-min(40, RuneCountInString(r))], " \t",
+					path)
+				t.Fail()
+			} else {
+				break
+			}
+		}
+	}
+	if t.Failed() {
+		t.Log(log.String())
+	}
+}
+
+func TestAllocRate(t *testing.T) {
+	t.Parallel()
+	for _, v := range [...]int{64, 128, 256, 512, 1024} {
+		var once, alloced, actual int
+		for i := 1e4; i > 0; i-- {
+			path := randPath(v)
+			dex := strings.LastIndexByte(path, Separator) + 1
+			alloc, ln := min(v>>3+v-dex, v), len(Simplify(path))
+			alloced += alloc
+			actual += ln
+			if alloc >= ln {
+				once++
+			}
+		}
+		t.Log(strconv.FormatFloat(float64(once)/1e2, 'f', 2, 64)+"%",
+			strconv.FormatFloat(float64(alloced)/float64(actual), 'f', 4, 64))
+	}
+}
+
+func BenchmarkAvgTrim(b *testing.B) {
+	const bestPath, worstPath = "\U0010FFFF", " "
+	fast := strings.Repeat(bestPath, b.N)
+	slow := strings.Repeat(worstPath, b.N/len(worstPath)+1)
+
+	b.SetBytes(int64(b.N/2 + 2))
+	b.ResetTimer()
+	for i := b.N; i > 0; i-- {
+		Trim(fast[:i], "", b.N)
+		Trim(slow[:i], " ", 2)
+	}
+}
+
+func BenchmarkRandTrim(b *testing.B) {
+	path := randPath(b.N)
+	n := RuneCountInString(path) - 1
+	b.SetBytes(int64(len(Trim(path, "…", n))))
+	b.ResetTimer()
+	for i := b.N; i > 0; i-- {
+		Trim(path, "…", n)
+	}
+}
+
+func BenchmarkAvgSimplify(b *testing.B) {
+	const bestPath = string(Separator)
+	const worstPath = string(Separator) + "\u07FF "
+	fast := strings.Repeat(bestPath, b.N)
+	slow := strings.Repeat(worstPath, b.N/len(worstPath)+1)
+
+	b.SetBytes(int64(b.N))
+	b.ResetTimer()
+	for i := b.N; i > 0; i-- {
+		Simplify(fast[:i])
+		Simplify(slow[:i])
+	}
+}
+
+func BenchmarkRandSimplify(b *testing.B) {
+	path := randPath(b.N)
+	b.SetBytes(int64(b.N))
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := b.N; i > 0; i-- {
+		Simplify(path)
+	}
+}
+
+func BenchmarkRandPath(b *testing.B) {
+	b.SetBytes(1)
+	randPath(b.N)
 }
